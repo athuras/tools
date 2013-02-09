3051061e8c815d0205c7959606fc9d94f9cb40c3
Alexander Huras <alex.huras@gmail.com>
2013-02-08 22:50:57 -0500

    refactor for OO

--- PatternRec/classifiers.py
+++ PatternRec/classifiers.py
@@ -5,7 +5,7 @@ from pdfs import Multivariate_Normal
 import auxPlot
 import numpy as np
 
-class PatternClass():
+class PatternClass(object):
     '''Container for training data for pattern recognition tasks
     currently hardcoded for normal distributions'''
 
@@ -40,7 +40,41 @@ class PatternClass():
             return zip(self.testing.min(0), self.testing.max(0))
 
 
-class MED_Classifier():
+class Supervised_Classifier(object):
+    ''''Virtual' Class, holds some logistical things like the training classes'''
+    def __init__(self, classes, *args, **kwargs):
+        assert len(classes) > 1
+        self.classes = classes
+        self.orderMap = dict((cls.name, i) for i, cls in enumerate(classes))
+        self.colorMap = ListedColormap([cls.colorCode for cls in classes])
+
+    def classify(self, x):
+        '''Naive classification, overload this!'''
+        return None
+
+    def iterRegion(self, bounds, splot, fn, h, **kwargs):
+        '''Execute fn across region, spray result to a splot object:
+        fn is passed a column vector'''
+        xx, yy = np.meshgrid(np.arange(bounds[0][0], bounds[0][1], h),
+                             np.arange(bounds[1][0], bounds[1][1], h))
+        Z = np.zeros((len(xx), len(yy)))
+        xn, yn = xx.shape
+        for ix in xrange(xn):
+            for iy in xrange(yn):
+                Z[ix, iy] = fn(np.c_[xx[ix, iy], yy[ix, iy]].T)
+        Z.reshape(xx.shape)
+
+        mode = None
+        if 'mode' in kwargs:
+            mode = kwargs['mode']
+
+        if mode is None:
+            splot.contour(xx, yy, Z, cmap=self.colorMap, linewidth=1)
+        else:
+            splot.pcolormesh(xx, yy, Z, cmap=self.colorMap, alpha=0.1)
+
+
+class MED_Classifier(Supervised_Classifier):
     '''
     Given a bunch (more than two) of PatternClass objects,
     trains a MED classifier. Some methods (such as classify) work with multi-
@@ -48,12 +82,10 @@ class MED_Classifier():
     '''
 
     def __init__(self, classes, *args, **kwargs):
-        assert len(classes) > 1  # Dirty error checking
-        self.tdata = [(i.name, i.mean) for i in classes]
+        super(MED_Classifier, self).__init__(classes, *args, **kwargs)
+        self.tdata = [(i.name, i.mean) for i in self.classes]
         self.rdata = [(i.name, np.average(i.training, axis=0))
-                      for i in classes]
-        self.orderMap = dict((n[0], i) for i, n in enumerate(self.tdata))
-        self.colorMap = ListedColormap([cls.colorCode for cls in classes])
+                      for i in self.classes]
 
     @classmethod
     def distance(cls, x, y):
@@ -67,42 +99,21 @@ class MED_Classifier():
         # TODO: Check if we're on the boundary (unlikely within double-precision)
         return self.orderMap[distances[0][0]]
 
-    def classifyBoundary(self, x, fuzz=0.1):
-        '''Identifies points on (or near) the decision boundary'''
-        distances = [MED_Classifier.distance(x, i[1]) for i in self.tdata]
-        distances.sort()
-        if abs(distances[0] - distances[1]) <= fuzz:
-            return 1
-        else:
-            return 0
-
-    def showRegions(self, bounds, splot, h=0.1, mode=None):
+    def showRegions(self, bounds, splot, h=0.1, **kwargs):
         '''Draws shaded regions over the bounds given, if mode
         is none, draws the boundaries, otherwise shades in regions
         '''
-        xx, yy = np.meshgrid(np.arange(bounds[0][0], bounds[0][1], h),
-                             np.arange(bounds[1][0], bounds[1][1], h))
-        # TODO: have classify take iterators as parameters.
-        Z = np.zeros((len(xx), len(yy)))
-        xn, yn = xx.shape
-        for ix in xrange(xn):
-            for iy in xrange(yn):
-                Z[ix, iy] = self.classify(np.c_[xx[ix, iy], yy[ix, iy]].T)
-        Z.reshape(xx.shape)
-        if mode is None:
-            splot.contour(xx, yy, Z, cmap=self.colorMap, linewidth=1)
-        else:
-            splot.pcolormesh(xx, yy, Z, cmap=self.colorMap, alpha=0.1)
+        super(MED_Classifier, self).iterRegion(bounds, splot, self.classify, h, **kwargs)
 
-class GEM_Classifier():
+
+class GEM_Classifier(Supervised_Classifier):
     '''Trained classifier using training points of several PatternClass
     objects'''
     def __init__(self, classes, *args, **kwargs):
         assert len(classes) > 1  # More Dirty error checking
+        super(GEM_Classifier, self).__init__(classes, *args, **kwargs)
         self.tdata = [(cls.name, cls.mean, np.linalg.inv(cls.cov))
-                      for cls in classes]
-        self.orderMap = dict((n[0], i) for i, n in enumerate(self.tdata))
-        self.colorMap = ListedColormap([cls.colorCode for cls in classes])
+                      for cls in self.classes]
 
     @classmethod
     def distance(cls, x, mean, inv):
@@ -118,41 +129,26 @@ class GEM_Classifier():
         distances.sort(key=lambda y: y[1])
         return distances[0][0]
 
-    def showRegions(self, bounds, splot, h=0.1, mode=None):
+    def showRegions(self, bounds, splot, h=0.1, **kwargs):
         '''Draws shaded regions over the bounds given, if mode
         is none, draws the boundaries, otherwise shades in regions
         '''
-        xx, yy = np.meshgrid(np.arange(bounds[0][0], bounds[0][1], h),
-                             np.arange(bounds[1][0], bounds[1][1], h))
-        Z = np.zeros((len(xx), len(yy)))
-        xn, yn = xx.shape
-        for ix in xrange(xn):
-            for iy in xrange(yn):
-                Z[ix, iy] = self.classify(np.c_[xx[ix, iy], yy[ix, iy]].T)
-        Z.reshape(xx.shape)
-        if mode is None:
-            splot.contour(xx, yy, Z, cmap=self.colorMap, linewidth=1)
-        else:
-            splot.pcolormesh(xx, yy, Z, cmap=self.colorMap, alpha=0.1)
+        super(GEM_Classifier, self).iterRegion(bounds, splot, self.classify, h, **kwargs)
 
-class MAP_Classifier():
+
+class MAP_Classifier(Supervised_Classifier):
     '''Trained Maximum A Priori classifier using training sets of PatternClass
     objects, assuming a Normal distribution centred at PatternClass.mean, with
     PatternClass.cov'''
     def __init__(self, classes, *args, **kwargs):
-        assert len(classes) > 1
+        super(MAP_Classifier, self).__init__(classes, *args, **kwargs)
         k = len(classes[0].mean)
-        # Dimensionality must match for ALL classes
         assert all(map(lambda p: len(p.mean) == k, classes))
-
-        priorSum = float(sum((len(cls.training) for cls in classes)))
+        priorSum = float(sum((len(cls.training) for cls in self.classes)))
         # (Name, prior, probability)
         self.tdata = [(cls.name, len(cls.training) / priorSum,
                       Multivariate_Normal(k, cls.mean, cls.cov))
-                      for cls in classes]
-
-        self.orderMap = dict((n[0], i) for i, n in enumerate(self.tdata))
-        self.colorMap = ListedColormap([cls.colorCode for cls in classes])
+                      for cls in self.classes]
 
     def classify(self, x):
         '''Fit x to one of the classes in the classifier'''
@@ -160,31 +156,19 @@ class MAP_Classifier():
         ps.sort(key=lambda z: z[1], reverse=True)
         return ps[0][0]
 
-    def showRegions(self, bounds, splot, h=0.1, mode='contour'):
+    def showRegions(self, bounds, splot, h=0.1, **kwargs):
         '''Draws shaded regions over the bounds given. If mode is none,
         draws nice shaded regions, otherwise gives contour (boundaries).'''
-        xx, yy = np.meshgrid(np.arange(bounds[0][0], bounds[0][1], h),
-                             np.arange(bounds[1][0], bounds[1][1], h))
-        Z = np.zeros((len(xx), len(yy)))
-        xn, yn = xx.shape
-        for ix in xrange(xn):
-            for iy in xrange(yn):
-                Z[ix, iy] = self.classify(np.c_[xx[ix, iy], yy[ix, iy]].T)
-        Z.reshape(xx.shape)
-        if mode == 'contour':
-            splot.contour(xx, yy, Z, cmap=self.colorMap, linewidth=1)
-        else:
-            splot.pcolormesh(xx, yy, Z, cmap=self.colorMap, alpha=0.1)
+        super(MAP_Classifier, self).iterRegion(bounds, splot, self.classify, h, **kwargs)
 
-class KNN_Classifier():
+
+class KNN_Classifier(Supervised_Classifier):
     '''Trains a K-Nearest-Neighbour classifier with a bunch of PatternClasses,
     Key difference from classical KNN: No voting, simply find the k-nearest
     neighbours from EACH class, and compute a class-subset mean-prototype.
     From there we then just use MED'''
     def __init__(self, classes, k, *args, **kwargs):
-        assert len(classes) > 1
-        self.classes = classes
-        self.colorMap = ListedColormap([cls.colorCode for cls in classes])
+        super(KNN_Classifier, self).__init__(classes, *args, **kwargs)
         self.k = k
 
     def classify(self, x):
@@ -202,19 +186,8 @@ class KNN_Classifier():
         d2.sort(key=lambda z: z[1])
         return d2[0][0]
 
-    def showRegions(self, bounds, splot, h=-1, mode='contour'):
+    def showRegions(self, bounds, splot, h=-1, **kwargs):
         '''Draws shaded regions over the bounds given. If mode isn't 'contour'
         draws nice shaded regions, otherwise gives the (you guessed it) contour
         '''
-        xx, yy = np.meshgrid(np.arange(bounds[0][0], bounds[0][1], h),
-                             np.arange(bounds[1][0], bounds[1][1], h))
-        Z = np.zeros((len(xx), len(yy)))
-        xn, yn = xx.shape
-        for ix in xrange(xn):
-            for iy in xrange(yn):
-                Z[ix, iy] = self.classify(np.c_[xx[ix, iy], yy[ix, iy]].T)
-        Z.reshape(xx.shape)
-        if mode == 'contour':
-            splot.contour(xx, yy, Z, cmap=self.colorMap, linewidth=1)
-        else:
-            splot.pcolormesh(xx, yy, Z, cmap=self.colorMap, alpha=0.4)
+        super(KNN_Classifier, self).iterRegion(bounds, splot, self.classify, h, **kwargs)
