8b25441c8c622d652c954b764d694a2c94939e72
Alexander Huras <alex.huras@gmail.com>
2013-10-19 14:31:36 -0400

    added harmony search w/ demo

---
+++ optimize/README.md
@@ -0,0 +1,111 @@
+
+# Harmony Search Demonstration
+
+Wherein we try to find global minima for three particularly hairy functions
+using Harmony Search
+
+
+    %pylab inline
+    import harmony
+    import numpy as np
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    
+    Welcome to pylab, a matplotlib-based Python environment [backend: module://IPython.zmq.pylab.backend_inline].
+    For more information, type 'help(pylab)'.
+
+
+## The Aforementioned Hairy Functions
+
+
+    def branin(x):
+        '''Vectorized Branin function'''
+        a = 5.1 / (4. * np.pi**2)
+        b = 5. / np.pi
+        c = 10 * (1 - 1. / (8 * np.pi))
+        return (((x[:, 1] - a) * x[:, 0]**2. + x[:, 0] * b - 6.)**2.
+               + np.cos(x[:, 0]) * c + 10)
+    
+    def griewank(x, fr=4000):
+        '''Vectorized Griewank'''
+        s = np.square(x).sum(axis=1)
+        p = np.ones_like(s)
+        for j in xrange(x.shape[1]):
+            p *= np.cos(x[:, j] / np.sqrt(j + 1))
+        
+        return s / fr - p + 1
+        
+    def mich(x, m=10):
+        '''Vectorized Michalewicz Function'''
+        m = 10;
+        s = np.zeros(shape=(x.shape[0]), dtype=x.dtype)
+        for i in xrange(x.shape[1]):
+            s += np.sin(x[:, i])*(np.sin((i + 1) * x[:, i]**2. / np.pi))**(2*m)
+        return -s
+
+
+    def grid_plot2d(f, bx, by, res=0.1):
+        '''Returns x, y, z. For use with plt.contour(x, y, z)'''
+        X, Y = np.arange(bx[0], bx[1], res), np.arange(by[0], by[1], res)
+        x, y = np.meshgrid(X, Y)
+        r = np.dstack((x, y))
+        r_2d = r.reshape((reduce(lambda a, b: a * b, r.shape[:-1]), r.shape[-1]))
+        return x, y, f(r_2d)
+
+
+    bx, by = (-5, 10), (-5, 10)
+    res = 0.2
+    x, y, z = grid_plot2d(branin, bx, by, res)
+    x2, y2, z2 = grid_plot2d(griewank, bx, by, res)
+    x3, y3, z3 = grid_plot2d(mich, bx, by, res)
+
+
+    f = plt.figure(figsize=(12, 4))
+    names = ('Branin Function', 'Griewank Function', 'Michalewicz')
+    levels = 100
+    for i, (a, b, c) in enumerate(((x, y, z), (x2, y2, z2), (x3, y3, z3))):
+        ax = f.add_subplot(1, 3, i + 1)
+        ax.contourf(a, b, c.reshape(a.shape), 100)
+        ax.set_title(names[i])
+
+
+![png](README_files/README_6_0.png)
+
+
+## Enter Harmony Search!
+
+For each function, we'll run a bunch of trials of Harmony Search, I've
+arbitrarily set the 'Memory Size' (hms) to 50, and the iterations to 350.
+Additionally, I've tuned the memory selection rate (gmcr) to 60%, this leads to
+a little more exploration (better for the Mchalewicz function). Because we're
+exploring more, I've also allowed the par to be a little higher, and the
+corresponding step size (fw) a little smaller to compensate.
+
+For each trial, I've plotted the solution as a (somewhat obnoxious) red 'X'. You
+can see that the global minima within the search range are located near the
+centroids of the  solution clusters. If we wanted to get *real*, we could then
+run a kmeans clustering routine to find the best solutions.
+
+
+    # Invert the signals to use hill-climbing HS.
+    funs = (lambda q: -branin(q), 
+            lambda q: -griewank(q),
+            lambda q: -mich(q))
+    trials = 150
+    bounds = (bx, by)
+    for i, a in enumerate(f.axes):
+        hs = harmony.HarmonySearch(funs[i], bounds, hms=50, max_iter=350, gmcr=0.6, par=0.25, fw=0.05)
+        s = [hs.execute() for t in xrange(trials)]
+        s = np.vstack(s)
+        a.scatter(s[:, 0], s[:, 1], marker='x', c='r', s=100.)
+
+
+    f
+
+
+
+
+![png](README_files/README_9_0.png)
+
+
---
+++ optimize/harmony.py
@@ -0,0 +1,102 @@
+import numpy as np
+import random
+import heapq as H
+
+class pTuple(tuple):
+    '''Subclass of tuple where '>' and '<' only look at the first element,
+    useful in a heapq list.'''
+    def __lt__(self, other):
+        return self[0] < other[0]
+    def __gt__(self, other):
+        return self[0] > other[0]
+
+class HarmonySearch(object):
+    '''Harmony Search without fret width discretization, set to Maximise'''
+    def __init__(self, f, bounds, constraints=[], hms=10, **kwargs):
+        '''Set the various parameters for the Harmony Search Metaheuristic
+        Optimization Method.
+        * f: the objective function to be MAXIMISED. input should be a 1xN array
+        * bounds: the region to be explored i.e. ((-10, 10), (10, 20)).
+        * constraints: an iterable of functions that have the following
+            signature-- (scalar, 1-d array) -> Bool
+        * hms: Harmony Memory Size, the size of the lookup heap
+
+        kwargs:
+        ** max_iter: The number of iterations to perform before exit
+        ** hmcr: Harmony Memory Criteria Rate, the probability that a solution
+            will be drawn from the existing bank
+        ** par: Pitch Adjustment Rate, given hmcr, the probability that the
+            memoized solution will be adjusted by the fret width (fw)
+        ** fw: Fret Width, broadcastable quantity to add/subtract from a
+            memoized solution to find a new solution
+        ** pitch_sequence: A generator that yields an infinite series of
+            booleans. Controls whether pitch is adjusted up or down,
+            Defaults to a flip-flop (True, False, True, False ...)
+
+        Example Usage:
+        def f(x):
+            return np.cos(x.sum(axis=1))
+
+        hs = HarmonySearch(f, ((-np.pi, pi), (-np.pi, pi)), hms=10)
+        hs.execute()
+        >> array([ 0.72568394, -0.76400488])
+        '''
+        self.hms = hms  # Harmony Memory Size
+        self.constraints = constraints
+        self.bounds = bounds
+        self.hm = None
+        self.f = f
+
+        # Additional Parameters
+        self.max_iter = kwargs.get('max_iter', 100)
+        self.fw = kwargs.get('fw', 0.1)
+        self.hmcr = kwargs.get('hmcr', 0.6)
+        self.par = kwargs.get('par', 0.05)
+        self.pitch_sequence = kwargs.get('pitch_sequence', HarmonySearch.alternator())
+
+    def random_selection(self):
+        return [random.uniform(i[0], i[1]) for i in self.bounds]
+
+    def random_selection_as_array(self):
+        return  np.hstack(self.random_selection())
+
+    @staticmethod
+    def alternator(i=True):
+        while True:
+            yield i
+            i = not i
+
+    def initial_improvise(self):
+        return [self.random_selection_as_array() for i in xrange(self.hms)]
+
+    def next_note(self):
+        '''Decide which 'note' to play next probabilistically,
+        returns (f(n), n) tuple.'''
+        d = random.uniform(0., 1.)
+        if d > self.hmcr:
+            s = self.random_selection_as_array()
+            return pTuple((self.f(s[None, :]), s))
+
+        z = random.randint(0, self.hms - 1)
+        if d < self.hmcr * self.par:
+            n = None
+            if next(self.pitch_sequence):
+                n = self.hm[z][1] + self.fw
+            else:
+                n = self.hm[z][1] - self.fw
+            return pTuple((self.f(n[None, :]), n))
+        else:
+            return self.hm[z]
+
+    def execute(self):
+        init_hm = self.initial_improvise()
+        hm = [pTuple((self.f(i[None, :]), i)) for i in init_hm]
+        H.heapify(hm)
+        self.hm = hm
+
+        for i in xrange(self.max_iter):
+            n = self.next_note()
+            if (n[0] >= self.hm[0][0] and all(z(n) for z in self.constraints)):
+                _ = H.heappushpop(self.hm, n)
+
+        return max(self.hm, key=lambda x: x[0])[1]
